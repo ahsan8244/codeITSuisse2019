0fa0726e2ed957d275506c17477619881a9f71ac
ahsan8244 <syed.ahsan8244@gmail.com>
2019-09-28 13:51:31 +0800

    add solution for dependency manager

--- routes/index.js
+++ routes/index.js
@@ -47,6 +47,61 @@ router.post('/sentiment-analysis', (req, res) => {
   res.json({ response: output });
 });
 
+router.post('/generateSequence', (req, res) => {
+  let input=req.body;
+  let modulesArr=input["modules"];
+  let depenPairs=input["dependencyPairs"];
+  
+  let dependees=[];
+  let dependents=[];
+  let mainObj={};
+  let main=[];
+
+  // Start off and create a moduleName to emptyList
+
+
+  function check(arr1, arr2){
+      for (var i=0, len=arr1.length; i<len; i++){
+          if (!(arr2.includes(arr1[i]))){
+              return false;
+          }
+      }
+      return true;
+  }
+
+  for(var i=0, len=depenPairs.length;i<len; i++){
+      dependee1=depenPairs[i]["dependee"];
+      dependent1=depenPairs[i]["dependentOn"];
+      dependees.push(dependee1);
+      dependents.push(dependent1);
+  }
+
+  for (var i=0, len=modulesArr.length; i<len; i++){
+      if (!(dependees.includes(modulesArr[i]))){
+          main.push(modulesArr[i]);
+      }else{
+          mainObj[modulesArr[i]]=[]
+      }
+  }
+  for(var i=0, len=depenPairs.length;i<len; i++){
+      dependee1=depenPairs[i]["dependee"];
+      dependent1=depenPairs[i]["dependentOn"];
+      mainObj[dependee1].push(dependent1);
+  }
+
+  for (var x=0, len=Object.keys(mainObj).length; x<len; x++){
+      for(var i=0, len=Object.keys(mainObj).length; i<len; i++){
+          if (check(mainObj[Object.keys(mainObj)[i]], main)){
+              if(!(main.includes(Object.keys(mainObj)[i]))){
+                  main.push(Object.keys(mainObj)[i]);
+              }
+          }
+      }
+  }
+
+  res.send(main);
+});
+
 const chessboard = (chessboard) => {
   let queen = []
 
