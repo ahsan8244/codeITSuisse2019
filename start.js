9a377235443ab013bbb198127161cef117977c20
ahsan8244 <syed.ahsan8244@gmail.com>
2019-09-28 10:55:29 +0800

    fix port issue

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const server = app.listen(3000, () => {
+const server = app.listen( process.env.PORT || 3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
