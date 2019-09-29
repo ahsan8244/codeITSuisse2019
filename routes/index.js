cebb6485351465115c10d486194fe56c00bfb0ae
ahsan8244 <syed.ahsan8244@gmail.com>
2019-09-29 10:55:05 +0800

    come on welvin

--- routes/index.js
+++ routes/index.js
@@ -312,121 +312,62 @@ router.post('/maximise_1c', (req, res) => {
 router.post('/maximise_1a', (req, res) => {
   let input = req.body;
 
-  function findMinRemaining(myCosts){
-      return Math.min(...myCosts);
-  }
-
-  function removeA(array, item) {
-      var index= array.indexOf(item);
-      if(index!=-1){
-          array.splice(index, 1);
-      }
-  }
-
-  function lastMove(myCosts, myMoney){
-      array=[...myCosts]
-      couldBe=[];
-      numNegatives=0;
-      for(var i=0; i<array.length; i++){
-          array[i]-=myMoney;
-          if(array[i]<=0){
-              numNegatives++;
-              couldBe.push(myCosts[i])
-          }
-      }
+  let capital = input.startingCapital;
+  let stocks = input.stocks;
+  const knapSack = function(W, wt, val, n) {  
+      if (n == 0 || W == 0)  
+          return 0;  
       
-      if(numNegatives>2){
-          return false;
-      }else{;
-          return couldBe;
-      }
+      // If weight of the nth item is more  
+      // than Knapsack capacity W, then  
+      // this item cannot be included 
+      // in the optimal solution  
+      if (wt[n-1] > W) {
+          return knapSack(W, wt, val, n-1);  
+      } 
+      else{ 
+          let left = val[n-1] + knapSack(W-wt[n-1], wt, val, n-1);
+          let right = knapSack(W, wt, val, n-1);
+          let maximum =  Math.max( left,right );
+          return maximum;
+      } 
+  }  
+
+  let stocksCost = stocks.map(item => item[2]);
+  let stockProfit = stocks.map(item => item[1]);
+  const profit = knapSack(capital,stocksCost,stockProfit,stocks.length);
+  let output = {
+      profit
+  };
+  let solution = [];
+  const subsetSum = function(L, n, result, m){
+  if(solution.length != 0){
+      return;
+  }
+  if(m == 0){
+      solution.push(result);
+      return;
   }
 
-  money=input["startingCapital"]
-  costs=[]
-
-  stocks=input["stocks"]
-
-  for (var i=0; i<stocks.length; i++){
-      profitPercent=stocks[i][1]/stocks[i][2]
-      stocks[i].push(profitPercent);
-      stocks[i].push(true);
-  }
-  stocks.sort(function(a,b){
-      return a[3]<b[3];
-  })
-
-  costMax=0;
-  profitMax=0;
-  newPortfolio=[]
-  for (var i=0; i<stocks.length; i++){
-      costs.push(stocks[i][2])
-      costMax+=stocks[i][2]
-      profitMax+=stocks[i][1]
-      newPortfolio.push(stocks[i][0])
+  if(n == 0){
+      return;
   }
 
-  console.log(stocks);
-
-
-  remainingMoney=money
-  profit=0;
-  portfolio=[]
-
-
-  minRemaining=findMinRemaining(costs);
-  console.log(remainingMoney);
-  console.log(minRemaining)
-  console.log(costs)
-
-  if(money<costMax){
-      while (remainingMoney>=minRemaining){
-          currentRoute=[]
-          for(var i=0; i<stocks.length; i++){
-              if(remainingMoney>=stocks[i][2] && stocks[i][4]){
-                  if(lastMove(costs, remainingMoney)){
-                      lastOnes=lastMove(costs, remainingMoney);
-                      for(var x=0; x<stocks.length; x++){
-                          for(var m=0; m<lastOnes.length; m++){
-                              if(lastOnes[m]===stocks[x][2]){
-                                  lastOnes[m]=stocks[x][1];
-                              }
-                          }
-                          lastOne=Math.max(...lastOnes)
-                          if(lastOne===stocks[x][1]){
-                              currentRoute.push(stocks[x][1])
-                              profit+=stocks[x][1];
-                              portfolio.push(stocks[x][0])
-                          }
-                      }
-                      remainingMoney=0;
-                  }else{
-                      stocks[i][4]=false;
-                      remainingMoney-=stocks[i][2];
-                      removeA(costs, stocks[i][2]);
-                      minRemaining=findMinRemaining(costs);
-                      profit+=stocks[i][1];
-                      currentRoute.push(stocks[i][1]);
-                      portfolio.push(stocks[i][0])
-                      console.log(remainingMoney);
-                      console.log(minRemaining)
-                      console.log(costs)
-                  }
-                  
-              }
-          }
-      }
-  }else{
-      profit=profitMax;
-      portfolio=newPortfolio;
+  if(L[n-1][1] <= m){
+      subsetSum(L,n-1,[...result,L[n-1]],m-L[n-1][1]);
   }
 
-  result={
-      "profit":profit,
-      "portfolio": portfolio,
+  subsetSum(L,n-1,result,m);
+
   }
-  console.log(result);
-  res.json(result);
+
+  subsetSum(stocks,stocks.length,[],output.profit);
+  let portfolio = solution[0].map(item => item[0]);
+  output = {
+      profit,
+      portfolio
+  }
+  res.json(output);
 });
 
 router.post('/gun-control', (req, res) => {
