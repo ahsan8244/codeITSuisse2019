5dd65ff104e81fb4640d621e011190632b22d292
Welvin <wewe2712@connect.hku.hk>
2019-09-29 10:51:39 +0800

    knapsack

---
+++ solutions/bankbranch.js
@@ -0,0 +1,10 @@
+let input = {
+"N": 6,
+"branch_officers_timings": [
+    2,
+    3
+]
+}
+let pos = input.N;
+let officers = input.branch_officers_timings;
+console.log(officers)
--- solutions/portmanagement1a.js
+++ solutions/portmanagement1a.js
@@ -1,5 +1,5 @@
 let input = {
-    "startingCapital": 400,
+    "startingCapital": 500,
     "stocks": [
         [
             "Sony", 
@@ -26,25 +26,54 @@ let input = {
 
 let capital = input.startingCapital;
 let stocks = input.stocks;
-
-let solution = []
-const subsetSum = function(L, n, result, m){
-    if(m == 0 || m ){
-      solution.push(result);
-      return;
-    }
+const knapSack = function(W, wt, val, n) {  
+    if (n == 0 || W == 0)  
+        return 0;  
     
-    if(n == 0){
-      return;
-    }
-  
-    if(L[n-1] <= m){
-      subsetSum(L,n-1,[...result,L[n-1]],m-L[n-1]);
-    }
-  
-    subsetSum(L,n-1,result,m);
-  
+    // If weight of the nth item is more  
+    // than Knapsack capacity W, then  
+    // this item cannot be included 
+    // in the optimal solution  
+    if (wt[n-1] > W) {
+        return knapSack(W, wt, val, n-1);  
+    } 
+    else{ 
+        let left = val[n-1] + knapSack(W-wt[n-1], wt, val, n-1);
+        let right = knapSack(W, wt, val, n-1);
+        let maximum =  Math.max( left,right );
+        return maximum;
+    } 
+}  
+
+let stocksCost = stocks.map(item => item[2]);
+let stockProfit = stocks.map(item => item[1]);
+const profit = knapSack(capital,stocksCost,stockProfit,stocks.length);
+let output = {
+    profit
+};
+let solution = [];
+const subsetSum = function(L, n, result, m){
+if(m == 0){
+    solution.push(result);
+    return;
 }
 
-subsetSum([1,2,3,4,5],5,[],12);
-console.log(solution);
+if(n == 0){
+    return;
+}
+
+if(L[n-1][1] <= m){
+    subsetSum(L,n-1,[...result,L[n-1]],m-L[n-1][1]);
+}
+
+subsetSum(L,n-1,result,m);
+
+}
+
+subsetSum(stocks,stocks.length,[],output.profit);
+let portfolio = solution[0].map(item => item[0]);
+output = {
+    profit,
+    portfolio
+}
+console.log(output);
